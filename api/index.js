99c6ba8c748a19a55f6452de0a1721258db9e19f
sigazen <shigazenanna@gmail.com>
2024-12-10 13:50:15 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -79,7 +79,7 @@ async function proxyRequest(req, res, apiKey) {
 }
 
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');*
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
